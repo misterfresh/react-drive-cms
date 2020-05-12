d09c8985e79689ae16cd396eb1736b03093bf4e0
Antoine <antoine.stollsteiner@projiris.fr>
2020-05-12 16:46:16 +0200

    correct bug

--- app/components/blocks/article.js
+++ app/components/blocks/article.js
@@ -7,6 +7,7 @@ let Article = ({ article, category }) => html`
             padding: 30px 0;
             display: block;
             border-bottom: solid 1px #f5f5f5;
+            width: 100%;
         }
         article .title {
             text-decoration: none;
