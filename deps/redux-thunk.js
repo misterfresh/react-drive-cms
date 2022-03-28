8be36d36ff89503a9df77fd82f7a5416a352c9f8
Antoine Pro <antoine.stollsteiner@sitroom.io>
2022-03-28 13:35:12 +0200

    remove unneeded dependencies

--- deps/redux-thunk.js
+++
@@ -1,7 +0,0 @@
-function createThunkMiddleware(t) {
-    return ({ dispatch: e, getState: n }) => (r) => (u) =>
-        'function' == typeof u ? u(e, n, t) : r(u)
-}
-const thunk = createThunkMiddleware()
-thunk.withExtraArgument = createThunkMiddleware
-export default thunk
